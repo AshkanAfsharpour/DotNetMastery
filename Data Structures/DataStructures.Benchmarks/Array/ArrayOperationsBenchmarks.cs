20583476178c6cd7a8c533224c2afe495de6e245
Ashkan <ashkan77af@gmail.com>
2024-10-20 21:02:05 +0330

    implemented Array operations benchmarks

---
+++ Data Structures/DataStructures.Benchmarks/Array/ArrayOperationsBenchmarks.cs
@@ -0,0 +1,52 @@
+﻿[MemoryDiagnoser]
+public class ArrayOperationsBenchmark
+{
+    private ArrayOperations<int> _arrayOps;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _arrayOps = new ArrayOperations<int>(10000);
+    }
+
+    [Benchmark]
+    public void AddBenchmark()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            _arrayOps.Add(i);
+        }
+    }
+
+    [Benchmark]
+    public void GetBenchmark()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            _arrayOps.Get(i % _arrayOps.Count);
+        }
+    }
+
+    [Benchmark]
+    public void UpdateBenchmark()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            _arrayOps.Update(i % _arrayOps.Count, i);
+        }
+    }
+
+    [Benchmark]
+    public void RemoveAtBenchmark()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            _arrayOps.Add(i);
+        }
+
+        for (int i = 9999; i >= 0; i--)
+        {
+            _arrayOps.RemoveAt(i);
+        }
+    }
+}
--- Data Structures/DataStructures.Benchmarks/Program.cs
+++ Data Structures/DataStructures.Benchmarks/Program.cs
@@ -1,6 +1,12 @@
-﻿BenchmarkRunner.Run<ArrayStackBenchmark>();
+﻿#region Array
+BenchmarkRunner.Run<ArrayOperationsBenchmark>();
+#endregion
+
+#region Stack
+BenchmarkRunner.Run<ArrayStackBenchmark>();
 BenchmarkRunner.Run<ListStackBenchmark>();
 BenchmarkRunner.Run<QueueStackBenchmark>();
 BenchmarkRunner.Run<LinkedListStackBenchmark>();
 BenchmarkRunner.Run<StackWrapperBenchmark>();
 BenchmarkRunner.Run<ConcurrentStackWrapperBenchmarks>();
+#endregion
