ac0c9bcf2a4a2b911051d84927f135b55ef14d43
Ashkan <ashkan77af@gmail.com>
2024-10-20 19:48:35 +0330

    implement thread-safe stack using c# built-in Concurrent Stack class

--- Data Structures/DataStructures.Benchmarks/Program.cs
+++ Data Structures/DataStructures.Benchmarks/Program.cs
@@ -3,3 +3,4 @@ BenchmarkRunner.Run<ListStackBenchmark>();
 BenchmarkRunner.Run<QueueStackBenchmark>();
 BenchmarkRunner.Run<LinkedListStackBenchmark>();
 BenchmarkRunner.Run<StackWrapperBenchmark>();
+BenchmarkRunner.Run<ConcurrentStackWrapperBenchmarks>();
---
+++ Data Structures/DataStructures.Core/Stack/ConcurrentStackWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Concurrent;
+
+public class ConcurrentStackWrapper<T>
+{
+    private readonly ConcurrentStack<T> _stack;
+
+    public ConcurrentStackWrapper()
+    {
+        _stack = new ConcurrentStack<T>();
+    }
+
+    public void Push(T item)
+    {
+        _stack.Push(item);
+    }
+
+    public T Pop()
+    {
+        if (_stack.TryPop(out var result))
+        {
+            return result;
+        }
+        throw new InvalidOperationException("Stack is empty");
+    }
+
+    public T Peek()
+    {
+        if (_stack.TryPeek(out var result))
+        {
+            return result;
+        }
+        throw new InvalidOperationException("Stack is empty");
+    }
+
+    public bool IsEmpty()
+    {
+        return _stack.IsEmpty;
+    }
+}
